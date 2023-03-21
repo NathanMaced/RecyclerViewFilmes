a68a564f52016804277398541227898198d55f71
NathanMaced <nathanmacedo02@gmail.com>
2023-03-21 19:05:04 -0300

    continuação

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.etecia.recyclerviewfilmes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,11 +11,16 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<Filmes> listaFilmes;
+//declarar o RecyclerView
+    RecyclerView idRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //apresentando o xml para o java
+        idRecyclerView = findViewById(R.id.idListaFilmes);
 
         //criando a base de dados para carregamento da lista de filmes
 
@@ -25,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         );
 
         //instanciando a classe adaptadora e passando os parâmetros
-        MyAdapter adapter = new MyAdapter(getApplicationContext(),listaFilmes);
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), listaFilmes);
+
+        //tipo de layout
     }
 }
