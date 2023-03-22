3dfb8fb6a626aea5716c3684260720782a91e7b2
NathanMaced <nathanmacedo02@gmail.com>
2023-03-21 21:45:13 -0300

    Tornando carview funcional com clique

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/ApresentaFIlmeActivity.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/ApresentaFIlmeActivity.java
@@ -2,13 +2,35 @@ package br.com.etecia.recyclerviewfilmes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ApresentaFIlmeActivity extends AppCompatActivity {
+
+    TextView mTitulo, mCategoria, mDescricao;
+
+    ImageView mImagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.apresenta_filme_layout);
+
+        mTitulo = findViewById(R.id.mTituloFilme);
+        mCategoria = findViewById(R.id.mCategoria);
+        mDescricao = findViewById(R.id.mDescricaoFilme);
+        mImagem = findViewById(R.id.mImagemFilme);
+
+        Intent intent = getIntent();
+        String titulo, descricao, categoria;
+        int imagemFilme;
+
+        titulo = intent.getExtras().getString("Titulo");
+        descricao = intent.getExtras().getString("Descricao");
+        categoria = intent.getExtras().getString("Categoria");
+
+        imagemFilme = intent.getExtras().getInt("ImagemFilme");
     }
 }
