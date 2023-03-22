474ee1ade8e328fc9a147d53243874a29227b966
NathanMaced <nathanmacedo02@gmail.com>
2023-03-21 21:34:24 -0300

    Tornando carview funcional com clique

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
@@ -1,6 +1,7 @@
 package br.com.etecia.recyclerviewfilmes;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,8 +45,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         holder.idCardFilmes.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                Intent intent = new Intent(mContexto, ApresentaFIlmeActivity.class);
 
+                intent.putExtra("Titulo", listaFilmes. get(position).getTitulo());
+                intent.putExtra("Descricao", listaFilmes. get(position).getDescricao());
+                intent.putExtra("Categoria", listaFilmes. get(position).getCategoria());
+                intent.putExtra("ImagemFilme", listaFilmes. get(position).getImagem());
+
+                mContexto.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             }
         });
     }
